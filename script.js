03e75566d8a02fc31d12548f773cbae72dd0be48
Journeyforgoodfoundation <meetdanvasc@gmail.com>
2026-06-05 13:59:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-let donationTotal = 3400000;
+const currentAmount = 80000;
+const goal = 150000;
 
-function updateDonations() {
-  const increase = Math.floor(Math.random() * 2000);
-  donationTotal += increase;
+document.getElementById("donationTotal").textContent =
+  "$" + currentAmount.toLocaleString();
 
-  document.getElementById("donationTotal").innerText =
-    "$" + donationTotal.toLocaleString();
-}
-
-setInterval(updateDonations, 3000);
+document.getElementById("progressText").textContent =
+  ((currentAmount / goal) * 100).toFixed(1) + "% of goal reached";
